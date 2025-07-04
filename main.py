8c006a20fbef5f43bf8f12ba7b0321b70c336841
vinayak957 <vinayakgupta182000@gmail.com>
2025-07-04 14:38:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,5 +59,7 @@ async def ban(ctx, member: discord.Member, *, reason="No reason"):
 async def clear(ctx, amount: int = 5):
     await ctx.channel.purge(limit=amount)
     await ctx.send(f"🧹 Cleared {amount} messages", delete_after=3)
+
 webservice()
+
 bot.run(os.environ['TOKEN'])
